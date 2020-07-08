9f70312897d07bddae3b36f1934d6b8ec7a3094b
Guillaume Noell <50482094+gn5@users.noreply.github.com>
2020-07-08 16:11:19 +0100

    Update make_singularity_image.sh

--- make_singularity_image.sh
+++ make_singularity_image.sh
@@ -6,4 +6,4 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
 quay.io/singularity/docker2singularity \
 mercury/indelible:6dbe32d72ad4ef707d0fbf66dac72aa7acdedfbc
 
-# singularity image will now be in ./singu dur
+# singularity image will now be in ./singu directory
